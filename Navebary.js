990a41cacc38e80efa4a2c06877a58121839fa7b
Sohaib Ahmad <sohaibahmad33888237@gmail.com>
2026-05-08 10:45:52 +0500

    Update Navebary.js

--- Navebary.js
+++ Navebary.js
@@ -1,21 +1,75 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Resturent.css'
 import { Link } from 'react-router-dom'
 
 const Navebary = () => {
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  const closeMenu = () => {
+    setMenuOpen(false)
+  }
+
   return (
     <div className='navebaar'>
-       <div>
+
+      <div>
         <h1>MARWAT RESTURENT</h1>
-       </div>
-       <div className='btns'>
-        <button><Link to={'/'} style={{textDecoration:"none"}}>HOME</Link></button>
-        {/* <button><Link to={'/about'} style={{textDecoration:"none", color:"black"}}>Our Story</Link></button> */}
-        <button><Link to={'/story'} style={{textDecoration:"none"}}>Our Story</Link></button>
-        <button><Link to={'/book_table'} style={{textDecoration:"none"}}>Book Table</Link></button>
-        <button><Link to={'/chef'} style={{textDecoration:"none"}}>Our Chef</Link></button>
-        <button><Link to={'gallery'} style={{textDecoration:"none"}}>GALLERY</Link></button>
-       </div>
+      </div>
+
+      <div
+        className='menu-icon'
+        onClick={() => setMenuOpen(!menuOpen)}
+      >☰</div>
+
+      <div className={`btns ${menuOpen ? 'active' : ''}`}>
+
+        <button onClick={closeMenu}>
+          <Link
+            to={'/'}
+            style={{ textDecoration: "none", color: "black" }}
+          >
+            HOME
+          </Link>
+        </button>
+
+        <button onClick={closeMenu}>
+          <Link
+            to={'/story'}
+            style={{ textDecoration: "none", color: "black" }}
+          >
+            Our Story
+          </Link>
+        </button>
+
+        <button onClick={closeMenu}>
+          <Link
+            to={'/book_table'}
+            style={{ textDecoration: "none", color: "black" }}
+          >
+            Book Table
+          </Link>
+        </button>
+
+        <button onClick={closeMenu}>
+          <Link
+            to={'/chef'}
+            style={{ textDecoration: "none", color: "black" }}
+          >
+            Our Chef
+          </Link>
+        </button>
+
+        <button onClick={closeMenu}>
+          <Link
+            to={'/gallery'}
+            style={{ textDecoration: "none", color: "black" }}
+          >
+            GALLERY
+          </Link>
+        </button>
+
+      </div>
+
     </div>
   )
 }
